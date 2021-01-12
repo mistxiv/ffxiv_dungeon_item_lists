c59119fabecef8f1c568786b66c34423be77a2f1
Mist <76016977+mistxiv@users.noreply.github.com>
2021-01-11 22:02:33 -0500

    refactor to fetch item id

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import requests
 import requests_cache
 from bs4 import BeautifulSoup
+import time
+import sys
 
 xivapi_key = os.getenv("XIVAPI_KEY")
 headers = {
@@ -16,19 +18,43 @@ ex_versions = [
 ]
 
 
-# Uses xivapi.com to determine if an item can be equipped
-def is_equippable(item_name):
-    # time.sleep(0.1)
+def xivapi_item(item_name):
+    print(f"Searching xivapi for \"{item_name}\"")
     xivapi_headers = {'user-agent': 'https://github.com/mistxiv'}
+    columns = [
+        'ID',
+        'EquipSlotCategory.ID',
+        'Name'
+    ]
     params = {
         'private_key': xivapi_key,
         'indexes': 'item',
         'string_algo': 'term',
-        'filters': 'EquipSlotCategory.ID>0',
+        'columns': ",".join(columns),
         'string': item_name
     }
     r = requests.get(f"https://xivapi.com/search", headers=xivapi_headers, params=params)
-    return r.json()["Pagination"]["ResultsTotal"] != 0
+    # Wait between requests if they're not answered from cache
+    if r.elapsed.total_seconds() > 0:
+        time.sleep(0.1)
+    results = r.json()['Results']
+    return results[0] if len(results) > 0 else None
+
+
+# Uses xivapi.com to determine if an item can be equipped
+def can_equip(item):
+    return item['EquipSlotCategory']['ID'] if item else False
+
+def lua_obj1_data_item(item):
+    return (
+        f'\t\t[{item["ID"]} = {{'
+        f'\t\t\t["Col"] = false,'
+        f'\t\t\t["Enabled"] = true,'
+        f'\t\t\t["HQ"] = true,'
+        f'\t\t\t["ItemName"] = "{item["Name"]}",'
+        f'\t\t\t["NQ"] = true,'
+        f'\t\t}},'
+    )
 
 
 def main():
@@ -45,7 +71,7 @@ def main():
             if drops_count > 0:
                 outfile = f"{p}/{name}.txt"
                 with open(outfile, "w") as f:
-                    f.write("\n".join(drops))
+                    f.write("\n".join(d["Name"] for d in drops))
                     print(f"Wrote file with {drops_count} equippable items: {outfile}")
 
 
@@ -62,14 +88,29 @@ def dungeon_urls(ex_version):
     return urls
 
 
-def dungeon_drops(url):
+def dungeon_item_names(url):
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     drops = set([d.string for d in soup.find_all("a", class_="db_popup tooltip_pos__right_space")])
-    drops = list([d for d in drops if is_equippable(d)])
+    drops = list(drops)
     drops.sort()
 
+    return drops
+
+
+def dungeon_drops(url):
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    item_names = set([d.string for d in soup.find_all("a", class_="db_popup tooltip_pos__right_space")])
+    item_names = list(item_names)
+    item_names.sort()
+    drops = []
+    for item_name in item_names:
+        item = xivapi_item(item_name)
+        if can_equip(item):
+            drops += [item]
     return drops
 
 
